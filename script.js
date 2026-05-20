a220cb8716911e6bd56cd2ab05fb54b5cc2a813c
yasminaburub <aburubyasmin@gmail.com>
2026-05-20 13:01:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+new Swiper('.card-wrapper', {
+    
+    loop: true,
+    spaceBetween:20,
+
+    // pagination bullet 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets:true 
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+   breakpoints :{
+       0: {
+           slidesPerView:1
+      },
+        768: {
+           slidesPerView: 2
+        },
+       1024: {
+           slidesPerView: 3
+       },
+    }
+});
